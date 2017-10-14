743b638cf547b951e9041b0042c540fe4d1c9285
Mitchell Caisse <computers1055@gmail.com>
2017-10-13 23:31:02 -0400

    Added some end points for car

--- CarTracker/CarTracker.API/Controllers/CarController.cs
+++ CarTracker/CarTracker.API/Controllers/CarController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarTracker.Common.Mappers;
 using CarTracker.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarTracker.API.Controllers
 {
 
-    [Route("api/car")]
+    [Route("api/car/")]
     public class CarController : Controller
     {
 
@@ -24,7 +25,39 @@ namespace CarTracker.API.Controllers
         public IActionResult Get(long id)
         {
             var car = _dbContext.Cars.FirstOrDefault(c => c.CarId == id);
-            return Ok(car);
+            if (null == car)
+            {
+                return NotFound();
+            }
+            return Ok(car.ToViewModel());
+        }
+
+        [HttpGet]
+        [Route("vin/{vin}")]
+        public IActionResult GetByVin(string vin)
+        {
+            var car = _dbContext.Cars.FirstOrDefault(c => c.Vin == vin);
+            if (null == car)
+            {
+                return NotFound();
+            }
+            return Ok(car.ToViewModel());
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAll()
+        {
+            var cars = _dbContext.Cars.ToList();
+            return Ok(cars.ToViewModel());
+        }
+
+        [HttpGet]
+        [Route("registered/{vin}")]
+        public IActionResult IsRegistered(string vin)
+        {
+            var exists = _dbContext.Cars.Any(c => c.Vin == vin);
+            return Ok(exists);
         }
     }
 }
