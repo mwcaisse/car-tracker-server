af960b878336154961aab194d774f8f1bd02fc20
Mitchell <computers1055@gmail.com>
2017-12-17 04:12:06 -0500

    Added the basis for ObdPid/Command Support enums

---
+++ CarTracker/CarTracker.Common/Enums/ObdCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarTracker.Common.Enums
+{
+    public enum ObdCommand
+    {
+        MonitorStatus = 1,
+        FreezeDtc = 2,
+        FuelSystemStatus = 3,
+        CalculatedEngineLoad = 4
+    }
+
+    public static class ObdCommandExtensions
+    {
+        public static string ToString(this ObdCommand command)
+        {
+            switch (command)
+            {
+                case ObdCommand.MonitorStatus:
+                    return "Monitor status since DTCs cleared";
+                default:
+                    return "";
+            }
+        }
+    }
+}
