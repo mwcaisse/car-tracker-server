575e542d0e8ef1bbe98f4e27e2cf8241c3deb138
Mitchell Caisse <computers1055@gmail.com>
2017-12-26 23:08:03 -0500

    Added a place service

---
+++ CarTracker/CarTracker.Common/Services/IPlaceService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarTracker.Common.Entities;
+using CarTracker.Common.Models.PlaceRequester;
+
+namespace CarTracker.Common.Services
+{
+    public interface IPlaceService
+    {
+
+        /// <summary>
+        /// Fetches the place with the given id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Place Get(long id);
+
+        /// <summary>
+        /// Fetches the place with the given google id
+        /// 
+        /// //TODO: Make this more generic. ForeignId?
+        /// </summary>
+        /// <param name="googleId"></param>
+        /// <returns></returns>
+        Place GetByGoogleId(string googleId);
+
+        /// <summary>
+        /// Fetches the place corresponding to the given PlaceModel. If one does not exists
+        ///     creates it.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        Place CreateOrGetPlace(IPlaceModel model);
+
+    }
+}
---
+++ CarTracker/CarTracker.Logic/Services/PlaceService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CarTracker.Common.Entities;
+using CarTracker.Common.Models.PlaceRequester;
+using CarTracker.Common.Services;
+using CarTracker.Data;
+
+namespace CarTracker.Logic.Services
+{
+    public class PlaceService : IPlaceService
+    {
+
+        private readonly CarTrackerDbContext _db;
+
+        public PlaceService(CarTrackerDbContext db)
+        {
+            this._db = db;
+        }
+
+        public Place Get(long id)
+        {
+            return _db.Places.FirstOrDefault(p => p.PlaceId == id);
+        }
+
+        public Place GetByGoogleId(string googleId)
+        {
+
+            return _db.Places.FirstOrDefault(p => p.GooglePlaceId == googleId);
+        }
+
+        public Place CreateOrGetPlace(IPlaceModel model)
+        {
+            var place = GetByGoogleId(model.Id);
+            if (null == place)
+            {
+                place = new Place()
+                {
+                    Latitude = Convert.ToDecimal(model.Location?.Latitude),
+                    Longitude = Convert.ToDecimal(model.Location?.Longitude),
+                    GooglePlaceId = model.Id,
+                    Name = model.Name                    
+                };
+
+                _db.Places.Add(place);
+                _db.SaveChanges();
+            }
+            return place;
+        }
+    }
+}
