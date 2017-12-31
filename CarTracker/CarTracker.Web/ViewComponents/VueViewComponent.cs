d5b93c9cb6b7fd64cd40e3cf344eff1e6fbf2620
Mitchell Caisse <computers1055@gmail.com>
2017-12-31 12:34:26 -0500

    Added a view component to render Vue views

---
+++ CarTracker/CarTracker.Web/ViewComponents/VueViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarTracker.Web.ViewComponents
+{
+    public class VueViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(string viewName)
+        {
+            var elm = new VueElement()
+            {
+                ViewName = viewName,
+                VmElementId = GetRandomViewElementId()
+            };
+            return View(elm);
+        }
+
+        protected string GetRandomViewElementId()
+        {
+            var guid = Guid.NewGuid();
+            return string.Format("pageVm_{0}", guid.ToString());
+        }
+
+        public class VueElement
+        {
+            public string ViewName { get; set; }
+            public string VmElementId { get; set; }
+        }
+    }
+}
