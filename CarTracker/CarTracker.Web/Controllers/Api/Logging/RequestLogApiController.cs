3423e635a3f7b73f5d411fa35fad7d2aa51e9d84
Mitchell <computers1055@gmail.com>
2018-02-01 21:27:48 -0500

    Added rough endpoints for server/request logging

---
+++ CarTracker/CarTracker.Web/Controllers/Api/Logging/RequestLogApiController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarTracker.Common.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarTracker.Web.Controllers.Api.Logging
+{
+    [Produces("application/json")]
+    [Route("api/log")]
+    public class RequestLogApiController : BaseApiController
+    {
+
+        [HttpGet]
+        [Route("request/{id}")]
+        public IActionResult Get(long id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("request/")]
+        public IActionResult GetAll(int skip = DefaultSkip, int take = DefaultTake, 
+            SortParam sort = null)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("event/{id}/request")]
+        public IActionResult GetByEventId(string id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ CarTracker/CarTracker.Web/Controllers/Api/Logging/ServerLogApiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarTracker.Common.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarTracker.Web.Controllers.Api.Logging
+{
+    [Produces("application/json")]
+    [Route("api/log/")]
+    public class ServerLogApiController : BaseApiController
+    {
+
+        [HttpGet]
+        [Route("server/{id}")]
+        public IActionResult Get(long id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("server/")]
+        public IActionResult GetAll(int skip = DefaultSkip,
+            int take = DefaultTake, SortParam sort = null)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("event/{id}/server")]
+        public IActionResult GetForEvent(string id, int skip = DefaultSkip, 
+            int take = DefaultTake, SortParam sort = null)
+        {
+            return Ok();
+        }
+ 
+    }
+}
