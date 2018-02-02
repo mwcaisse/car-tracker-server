e38f99b7cebb2901c8ad54304987d8ed283c3a21
Mitchell <computers1055@gmail.com>
2018-02-01 21:33:41 -0500

    Added Request/Server log view models

---
+++ CarTracker/CarTracker.Common/ViewModels/Logging/RequestLogViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarTracker.Common.Enums;
+using CarTracker.Common.ViewModels.Auth;
+
+namespace CarTracker.Common.ViewModels.Logging
+{
+    public class RequestLogViewModel
+    {
+        public long RequestLogId { get; set; }
+        public Guid RequestUuid { get; set; }
+        public LogType Type { get; set; }
+        public string ClientAddress { get; set; }
+        public string RequestUrl { get; set; }
+        public string RequestMethod { get; set; }
+        public string RequestHeaders { get; set; }
+        public string RequestBody { get; set; }
+        public string ResponseStatus { get; set; }
+        public string ResponseHeaders { get; set; }
+        public string ResponseBody { get; set; }
+        public DateTime CreateDate { get; set; }
+        public long? UserId { get; set; }
+
+        public UserViewModel User { get; set; }
+    }
+}
---
+++ CarTracker/CarTracker.Common/ViewModels/Logging/ServerLogVIewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarTracker.Common.Enums;
+
+namespace CarTracker.Common.ViewModels.Logging
+{
+    public class ServerLogVIewModel
+    {
+
+        public long ServerLogId { get; set; }
+        public Guid RequestUuid { get; set; }
+        public LogType Type { get; set; }
+        public string Message { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string StackTrace { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
