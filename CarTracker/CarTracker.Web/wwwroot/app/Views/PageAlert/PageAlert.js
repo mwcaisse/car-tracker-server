8f9fe8b1b25628c06be5c4a2fc48b750231f0add
Mitchell <computers1055@gmail.com>
2018-02-15 19:03:47 -0500

    Added PageAlert module to the common view

---
+++ CarTracker/CarTracker.Web/wwwroot/app/Views/PageAlert/PageAlert.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+define("Views/PageAlert/PageAlert",
+    ["Service/util",
+        "AMD/text!Views/PageAlert/PageAlert.html",
+        "Components/Common/PageAlert/PageAlert"],
+    function (util, template) {
+
+        var vm = function (elementId) {
+
+            return new Vue({
+                el: elementId,
+                template: template
+            });
+
+        };
+
+        return vm;
+
+    });
