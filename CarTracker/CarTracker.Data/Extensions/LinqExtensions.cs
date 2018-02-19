29cdb119edbb2907560b444f5cf63134e0c35e55
Mitchell Caisse <computers1055@gmail.com>
2018-02-19 10:21:48 -0500

    Genericized the filters to allow them to work on any IQueryable

--- CarTracker/CarTracker.Data/Extensions/LinqExtensions.cs
+++ CarTracker/CarTracker.Data/Extensions/LinqExtensions.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
+using CarTracker.Common;
 using CarTracker.Common.Entities;
 using CarTracker.Common.Exceptions;
 using CarTracker.Common.ViewModels;
@@ -96,6 +97,81 @@ namespace CarTracker.Data.Extensions
         public static IQueryable<T> Active<T>(this IQueryable<T> query) where T : IActiveEntity
         {
             return query.Where(e => e.Active);
+        }
+
+        public static Expression<Func<T, bool>> ConstructExpression<T>(string propertyName, string operationName, string value)
+        {
+            var param = Expression.Parameter(typeof(T), "rl");
+            var left = Expression.Property(param, propertyName);
+            var right = Expression.Constant(value, typeof(string));
+
+            var propertyType = typeof(T).GetProperty(propertyName).PropertyType;
+            if (propertyType == typeof(DateTime))
+            {
+                var dateValue = DateTimeOffset.FromUnixTimeMilliseconds(Convert.ToInt64(value)).DateTime;
+                right = Expression.Constant(dateValue, typeof(DateTime));
+            }
+            var exp = Expression.Lambda<Func<T, bool>>(
+                ConstructExpressionCondition(operationName, left, right), param
+            );
+            return exp;
+        }
+
+        public static Expression ConstructExpressionCondition(string operationName,
+            MemberExpression left, ConstantExpression right)
+        {
+
+            if (string.Equals(Constants.FilterOperation.Ne, operationName, StringComparison.OrdinalIgnoreCase))
+            {
+                return Expression.NotEqual(left, right);
+            }
+            if (string.Equals(Constants.FilterOperation.Lte, operationName, StringComparison.OrdinalIgnoreCase))
+            {
+                return Expression.LessThan(left, right);
+            }
+            if (string.Equals(Constants.FilterOperation.Gte, operationName, StringComparison.OrdinalIgnoreCase))
+            {
+                return Expression.GreaterThanOrEqual(left, right);
+            }
+            if (string.Equals(Constants.FilterOperation.Lt, operationName, StringComparison.OrdinalIgnoreCase))
+            {
+                return Expression.LessThan(left, right);
+            }
+            if (string.Equals(Constants.FilterOperation.Gt, operationName, StringComparison.OrdinalIgnoreCase))
+            {
+                return Expression.GreaterThan(left, right);
+            }
+            if (string.Equals(Constants.FilterOperation.Cont, operationName, StringComparison.OrdinalIgnoreCase))
+            {
+                //construct the contains method
+                MethodInfo method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+                return Expression.Call(left, method, right);
+            }
+
+            //Default to equals
+            return Expression.Equal(left, right);
+        }
+
+
+        public static IQueryable<T> Filter<T>(this IQueryable<T> query,
+            Dictionary<string, string> filters)
+        {
+            foreach (var pair in filters)
+            {
+                var tokens = pair.Key.Split("__");
+                if (tokens.Length != 2)
+                {
+                    //ignore any filter if it isn't formatted correctly
+                    continue;
+                }
+
+                var propertyName = tokens[0];
+                var operatorName = tokens[1];
+
+                query = query.Where(ConstructExpression<T>(propertyName, operatorName, pair.Value));
+            }
+
+            return query;
         }
     }
 }
