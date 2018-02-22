6cb3a77d7d2067dfe76a7052be4bab502c4c2109
Mitchell <computers1055@gmail.com>
2018-02-22 18:05:49 -0500

    WIP TripSummary view model

---
+++ CarTracker/CarTracker.Common/ViewModels/TripSummaryViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarTracker.Common.ViewModels
+{
+    public class TripSummaryViewModel
+    {
+        public DateTime? PeriodStart { get; set; }
+
+        public DateTime? PeriodEnd { get; set; }
+
+        public double MilesDriven
+        {
+            get { return Trips.Select(t => t.DistanceTraveled).Sum() ?? 0; }
+        }
+
+        public long NumberOfTrips => Trips.Count();
+
+        public IEnumerable<PlaceViewModel> PlacedVisited { get; set; }
+
+        public IEnumerable<PlaceViewModel> NewPlacesVisited { get; set; }
+
+        public IEnumerable<TripViewModel> Trips { get; set; }
+
+
+
+    }
+}
