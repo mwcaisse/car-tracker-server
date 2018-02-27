7f679f1622cbb22bb71b2f9ab2ffda9616a9496f
Mitchell <computers1055@gmail.com>
2018-02-27 18:44:34 -0500

    Removed TripSummaryViewModel. Moved it to a Model instead

---
+++ CarTracker/CarTracker.Common/Models/TripSummaryModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarTracker.Common.Entities;
+
+namespace CarTracker.Common.Models
+{
+    public class TripSummaryModel
+    {
+
+        public DateTime? PeriodStart { get; set; }
+
+        public DateTime? PeriodEnd { get; set; }
+
+        public decimal MilesDriven{ get; set; }
+
+        public IEnumerable<Trip> Trips { get; set; }
+
+        public IEnumerable<Place> PlacesVisited { get; set; }
+
+        public IEnumerable<Place> NewPlacesVisited { get; set; }
+  
+    }
+}
