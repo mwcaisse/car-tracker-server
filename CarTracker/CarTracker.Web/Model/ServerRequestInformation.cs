edf4b61a7297ef8484c652b4a6d1fc4896d6650a
Mitchell <computers1055@gmail.com>
2018-04-11 19:16:53 -0400

    IRequestInformationbehaves properly if there is no HttpContext

--- CarTracker/CarTracker.Web/Model/ServerRequestInformation.cs
+++ CarTracker/CarTracker.Web/Model/ServerRequestInformation.cs
@@ -11,68 +11,33 @@ namespace CarTracker.Web.Model
     public class ServerRequestInformation : IRequestInformation
     {
 
-        private long? _userId;
+        public bool IsAuthenticated { get; }
 
-        public bool IsAuthenticated => _httpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
+        public long? UserId { get; }
 
-        public long? UserId
-        {
-            get
-            {
-                if (!_userId.HasValue)
-                {
-                    if (IsAuthenticated)
-                    {
-                        var userIdClaim = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Sid);
-                        _userId = Convert.ToInt64(userIdClaim.Value);
-                    }
-                    else
-                    {
-                        _userId = -1;
-                    }
-                }
-                if (_userId.Value < 0)
-                {
-                    return null;
-                }
-                return _userId.Value;
-            }
-        }
+        public string Username { get; }
 
-        private string _username;
-
-        public string Username
-        {
-            get
-            {
-                if (string.IsNullOrWhiteSpace(_username) && IsAuthenticated)
-                {
-                    var usernameClaim = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Name);
-                    _username = usernameClaim?.Value;
-                }  
-                return _username;
-            }
-        }
-
-        private string _clientAddress;
-
-        public string ClientAddress
-        {
-            get
-            {
-                if (string.IsNullOrWhiteSpace(_clientAddress))
-                {
-                    _clientAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
-                }
-                return _clientAddress;
-            }
-        }
-
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        public string ClientAddress { get; }
 
         public ServerRequestInformation(IHttpContextAccessor httpContextAccessor)
         {
-            this._httpContextAccessor = httpContextAccessor;
+            if (null != httpContextAccessor.HttpContext)
+            {
+                IsAuthenticated = httpContextAccessor.HttpContext.User.Identity.IsAuthenticated;
+                ClientAddress = httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+                Username = null;
+                UserId = null;
+
+                if (IsAuthenticated)
+                {
+                    var usernameClaim = httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Name);
+                    Username = usernameClaim?.Value;
+
+                    var userIdClaim = httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Sid);
+                    UserId = Convert.ToInt64(userIdClaim.Value);
+                }
+            }
         }
     }
 }
